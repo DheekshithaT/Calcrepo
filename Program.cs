c2948ad310a4e763886739e420e9b22c314ec542
dixie354 <dheekshithaduri2@gmail.com>
2024-02-16 10:48:34 +0530

    division

--- Program.cs
+++ Program.cs
@@ -5,3 +5,7 @@ Console.WriteLine("Add Call");
 calc.Add(12, 45);
 Console.WriteLine("Multiply call");
 calc.Mul(10.5,12.45);
+Console.WriteLine("Division call");
+calc.Div(20,4);
+Console.WriteLine("Multiply call");
+calc.Sub(20,4);
